918ad699c98f43c5dde348a434c68c2df62b73b0
Nadav Sinai <nadav@algotec.co.il>
2021-12-27 18:29:20 +0200

    I like hacking around webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
 const {styles} = require('@ckeditor/ckeditor5-dev-utils')
-module.exports =
-    {
-        module: {
-            rules: [
+module.exports = function (config, options, targetOptions) {
+    config.module.rules.push(
+        {
+            test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+            use: ['raw-loader']
+        },
+        {
+            test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+            use: [
                 {
-                    test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-                    use: ['raw-loader']
-                },
-                {
-                    test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
-                    use: [
-                        {
-                            loader: './node_modules/@ckeditor/ckeditor5-dev-utils/node_modules/postcss-loader/src/index.js',
-                            options:
-                                styles.getPostCssConfig({
-                                    themeImporter: {
-                                        themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
-                                    },
-                                    minify: false
-                                })
-
+                    loader: 'style-loader',
+                    options: {
+                        injectType: 'singletonStyleTag',
+                        attributes: {
+                            'data-cke': true
                         }
-                    ]
+                    }
                 },
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: styles.getPostCssConfig({
+                            themeImporter: {
+                                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                            },
+                            minify: false
+                        })
+                    }
+                }
             ]
-        }
-    };
+        },
+    )
+    ;
+///hack hack hack. - we force angular not to process the css that has ckeditor5 in its name
+    const cssRule = config.module.rules.find(x => x.test.exec && x.test.exec('.css'));
+    cssRule.exclude = /ckeditor5/;
+    return config;
+}
+;
